6400f70d282468fac1be0c9de63b6888b2fbcb34
yungpanko <jrjohns@mit.edu>
2017-07-17 17:01:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Code goes here
+class MovieCard extends React.Component {
+  render() {
+    // ... The render stuff from before
+  }
+}
+
+MovieCard.defaultProps = {
+  poster: 'http://i.imgur.com/bJw8ndW.png'
+};
+
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    genres={jurassicWorldGenres}
+  />,
+  document.getElementById('root')
+);
